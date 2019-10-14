de0b0e572510bde2581169f4f48746699a2a4a6a
Erik Unger <erik@erikunger.com>
2019-10-14 10:46:14 +0200

    renamed With<n>CallParams to With<n>FuncParams

--- withstackparams.go
+++ withstackparams.go
@@ -41,77 +41,77 @@ func WithFuncParams(resultVar *error, params ...interface{}) {
 	*resultVar = newWithFuncParams(*resultVar, params...)
 }
 
-func With0CallParams(resultVar *error) {
+func With0FuncParams(resultVar *error) {
 	if *resultVar == nil {
 		return
 	}
 	*resultVar = newWithFuncParams(*resultVar)
 }
 
-func With1CallParams(resultVar *error, p0 interface{}) {
+func With1FuncParams(resultVar *error, p0 interface{}) {
 	if *resultVar == nil {
 		return
 	}
 	*resultVar = newWithFuncParams(*resultVar, p0)
 }
 
-func With2CallParams(resultVar *error, p0, p1 interface{}) {
+func With2FuncParams(resultVar *error, p0, p1 interface{}) {
 	if *resultVar == nil {
 		return
 	}
 	*resultVar = newWithFuncParams(*resultVar, p0, p1)
 }
 
-func With3CallParams(resultVar *error, p0, p1, p2 interface{}) {
+func With3FuncParams(resultVar *error, p0, p1, p2 interface{}) {
 	if *resultVar == nil {
 		return
 	}
 	*resultVar = newWithFuncParams(*resultVar, p0, p1, p2)
 }
 
-func With4CallParams(resultVar *error, p0, p1, p2, p3 interface{}) {
+func With4FuncParams(resultVar *error, p0, p1, p2, p3 interface{}) {
 	if *resultVar == nil {
 		return
 	}
 	*resultVar = newWithFuncParams(*resultVar, p0, p1, p2, p3)
 }
 
-func With5CallParams(resultVar *error, p0, p1, p2, p3, p4 interface{}) {
+func With5FuncParams(resultVar *error, p0, p1, p2, p3, p4 interface{}) {
 	if *resultVar == nil {
 		return
 	}
 	*resultVar = newWithFuncParams(*resultVar, p0, p1, p2, p3, p4)
 }
 
-func With6CallParams(resultVar *error, p0, p1, p2, p3, p4, p5 interface{}) {
+func With6FuncParams(resultVar *error, p0, p1, p2, p3, p4, p5 interface{}) {
 	if *resultVar == nil {
 		return
 	}
 	*resultVar = newWithFuncParams(*resultVar, p0, p1, p2, p3, p4, p5)
 }
 
-func With7CallParams(resultVar *error, p0, p1, p2, p3, p4, p5, p6 interface{}) {
+func With7FuncParams(resultVar *error, p0, p1, p2, p3, p4, p5, p6 interface{}) {
 	if *resultVar == nil {
 		return
 	}
 	*resultVar = newWithFuncParams(*resultVar, p0, p1, p2, p3, p4, p5, p6)
 }
 
-func With8CallParams(resultVar *error, p0, p1, p2, p3, p4, p5, p6, p7 interface{}) {
+func With8FuncParams(resultVar *error, p0, p1, p2, p3, p4, p5, p6, p7 interface{}) {
 	if *resultVar == nil {
 		return
 	}
 	*resultVar = newWithFuncParams(*resultVar, p0, p1, p2, p3, p4, p5, p6, p7)
 }
 
-func With9CallParams(resultVar *error, p0, p1, p2, p3, p4, p5, p6, p7, p8 interface{}) {
+func With9FuncParams(resultVar *error, p0, p1, p2, p3, p4, p5, p6, p7, p8 interface{}) {
 	if *resultVar == nil {
 		return
 	}
 	*resultVar = newWithFuncParams(*resultVar, p0, p1, p2, p3, p4, p5, p6, p7, p8)
 }
 
-func With10CallParams(resultVar *error, p0, p1, p2, p3, p4, p5, p6, p7, p8, p9 interface{}) {
+func With10FuncParams(resultVar *error, p0, p1, p2, p3, p4, p5, p6, p7, p8, p9 interface{}) {
 	if *resultVar == nil {
 		return
 	}
