4767799a3db7b032031cfd44d336f277b935bd8f
Erik Unger <erik@erikunger.com>
2019-10-18 13:49:14 +0200

    add functions for recovering and wrapping panics

---
+++ panics.go
@@ -0,0 +1,63 @@
+package wraperr
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Logger interface {
+	Printf(format string, args ...interface{})
+}
+
+func LogPanicWithFuncParams(log Logger, params ...interface{}) {
+	p := recover()
+	if p == nil {
+		return
+	}
+
+	err := newWithFuncParamsSkip(1, AsError(p), params...)
+
+	log.Printf("LogPanicWithFuncParams: %s", err.Error())
+
+	panic(p)
+}
+
+func RecoverAndLogPanicWithFuncParams(log Logger, params ...interface{}) {
+	p := recover()
+	if p == nil {
+		return
+	}
+
+	err := newWithFuncParamsSkip(1, AsError(p), params...)
+
+	log.Printf("RecoverAndLogPanicWithFuncParams: %s", err.Error())
+}
+
+func RecoverPanicAsErrorWithFuncParams(resultVar *error, params ...interface{}) {
+	p := recover()
+	if p == nil {
+		return
+	}
+
+	err := newWithFuncParamsSkip(1, AsError(p), params...)
+
+	if *resultVar != nil {
+		// TODO wrap existing error result to not loose it
+	}
+	*resultVar = err
+}
+
+func AsError(val interface{}) error {
+	switch x := val.(type) {
+	case nil:
+		return nil
+	case error:
+		return x
+	case string:
+		return errors.New(x)
+	case fmt.Stringer:
+		return errors.New(x.String())
+	default:
+		return fmt.Errorf("%+v", val)
+	}
+}
--- withstackparams.go
+++ withstackparams.go
@@ -9,7 +9,7 @@ Could only be parsed from runtime.Stack() result text.
 See https://www.ardanlabs.com/blog/2015/01/stack-traces-in-go.html
 */
 
-func newWithFuncParams(err error, params ...interface{}) *withStackParams {
+func newWithFuncParamsSkip(skip int, err error, params ...interface{}) *withStackParams {
 	switch w := err.(type) {
 	case callStackParamsProvider:
 		// OK, wrap the wrapped
@@ -28,81 +28,87 @@ func newWithFuncParams(err error, params ...interface{}) *withStackParams {
 	return &withStackParams{
 		withStack: withStack{
 			err:   err,
-			stack: callStack(2),
+			stack: callStack(skip + 1),
 		},
 		params: params,
 	}
 }
 
+func WithFuncParamsSkip(skip int, resultVar *error, params ...interface{}) {
+	if *resultVar != nil {
+		*resultVar = newWithFuncParamsSkip(1+skip, *resultVar, params...)
+	}
+}
+
 func WithFuncParams(resultVar *error, params ...interface{}) {
 	if *resultVar != nil {
-		*resultVar = newWithFuncParams(*resultVar, params...)
+		*resultVar = newWithFuncParamsSkip(1, *resultVar, params...)
 	}
 }
 
 func With0FuncParams(resultVar *error) {
 	if *resultVar != nil {
-		*resultVar = newWithFuncParams(*resultVar)
+		*resultVar = newWithFuncParamsSkip(1, *resultVar)
 	}
 }
 
 func With1FuncParams(resultVar *error, p0 interface{}) {
 	if *resultVar != nil {
-		*resultVar = newWithFuncParams(*resultVar, p0)
+		*resultVar = newWithFuncParamsSkip(1, *resultVar, p0)
 	}
 }
 
 func With2FuncParams(resultVar *error, p0, p1 interface{}) {
 	if *resultVar != nil {
-		*resultVar = newWithFuncParams(*resultVar, p0, p1)
+		*resultVar = newWithFuncParamsSkip(1, *resultVar, p0, p1)
 	}
 }
 
 func With3FuncParams(resultVar *error, p0, p1, p2 interface{}) {
 	if *resultVar != nil {
-		*resultVar = newWithFuncParams(*resultVar, p0, p1, p2)
+		*resultVar = newWithFuncParamsSkip(1, *resultVar, p0, p1, p2)
 	}
 }
 
 func With4FuncParams(resultVar *error, p0, p1, p2, p3 interface{}) {
 	if *resultVar != nil {
-		*resultVar = newWithFuncParams(*resultVar, p0, p1, p2, p3)
+		*resultVar = newWithFuncParamsSkip(1, *resultVar, p0, p1, p2, p3)
 	}
 }
 
 func With5FuncParams(resultVar *error, p0, p1, p2, p3, p4 interface{}) {
 	if *resultVar != nil {
-		*resultVar = newWithFuncParams(*resultVar, p0, p1, p2, p3, p4)
+		*resultVar = newWithFuncParamsSkip(1, *resultVar, p0, p1, p2, p3, p4)
 	}
 }
 
 func With6FuncParams(resultVar *error, p0, p1, p2, p3, p4, p5 interface{}) {
 	if *resultVar != nil {
-		*resultVar = newWithFuncParams(*resultVar, p0, p1, p2, p3, p4, p5)
+		*resultVar = newWithFuncParamsSkip(1, *resultVar, p0, p1, p2, p3, p4, p5)
 	}
 }
 
 func With7FuncParams(resultVar *error, p0, p1, p2, p3, p4, p5, p6 interface{}) {
 	if *resultVar != nil {
-		*resultVar = newWithFuncParams(*resultVar, p0, p1, p2, p3, p4, p5, p6)
+		*resultVar = newWithFuncParamsSkip(1, *resultVar, p0, p1, p2, p3, p4, p5, p6)
 	}
 }
 
 func With8FuncParams(resultVar *error, p0, p1, p2, p3, p4, p5, p6, p7 interface{}) {
 	if *resultVar != nil {
-		*resultVar = newWithFuncParams(*resultVar, p0, p1, p2, p3, p4, p5, p6, p7)
+		*resultVar = newWithFuncParamsSkip(1, *resultVar, p0, p1, p2, p3, p4, p5, p6, p7)
 	}
 }
 
 func With9FuncParams(resultVar *error, p0, p1, p2, p3, p4, p5, p6, p7, p8 interface{}) {
 	if *resultVar != nil {
-		*resultVar = newWithFuncParams(*resultVar, p0, p1, p2, p3, p4, p5, p6, p7, p8)
+		*resultVar = newWithFuncParamsSkip(1, *resultVar, p0, p1, p2, p3, p4, p5, p6, p7, p8)
 	}
 }
 
 func With10FuncParams(resultVar *error, p0, p1, p2, p3, p4, p5, p6, p7, p8, p9 interface{}) {
 	if *resultVar != nil {
-		*resultVar = newWithFuncParams(*resultVar, p0, p1, p2, p3, p4, p5, p6, p7, p8, p9)
+		*resultVar = newWithFuncParamsSkip(1, *resultVar, p0, p1, p2, p3, p4, p5, p6, p7, p8, p9)
 	}
 }
 
