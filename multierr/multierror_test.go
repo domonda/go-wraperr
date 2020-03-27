28c17d618860094160742b1950e659b85015fca8
Erik Unger <erik@erikunger.com>
2020-03-27 15:51:10 +0100

    moved Combine and MultiError to multierr sub pkg

--- multierr/multierror_test.go
+++ multierr/multierror_test.go
@@ -1,4 +1,4 @@
-package wraperr
+package multierr
 
 import (
 	"errors"
--- withfuncparams_test.go
+++ withfuncparams_test.go
@@ -38,9 +38,9 @@ func ExampleWithFuncParams() {
 	// Output:
 	// error in funcC
 	// github.com/domonda/go-wraperr.funcC()
-	//     github.com/domonda/go-wraperr/withstackparams_test.go:24
+	//     github.com/domonda/go-wraperr/withfuncparams_test.go:24
 	// github.com/domonda/go-wraperr.funcB([]string{"Hello World!", "X\nX"})
-	//     github.com/domonda/go-wraperr/withstackparams_test.go:18
+	//     github.com/domonda/go-wraperr/withfuncparams_test.go:18
 	// github.com/domonda/go-wraperr.funcA(666, "Hello World!")
-	//     github.com/domonda/go-wraperr/withstackparams_test.go:12
+	//     github.com/domonda/go-wraperr/withfuncparams_test.go:12
 }
